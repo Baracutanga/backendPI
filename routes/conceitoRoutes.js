e8c6946f73b4f2612bae52b582a9322dff0cb0e6
Roger Cauã <132178311+rogercaua@users.noreply.github.com>
2024-10-10 08:16:07 -0300

    Update conceitoRoutes.js

--- routes/conceitoRoutes.js
+++ routes/conceitoRoutes.js
@@ -4,10 +4,10 @@ const conceitoController = require("../controllers/conceitoController");
 const autenticaMiddlewareProfessor = require("../middleware/autenticaMiddlewareProfessor")
 const autenticaMiddlewareCoordenador = require("../middleware/autenticaMiddlewareCoordenador")
 
-router.get("/turmadisciplina",  conceitoController.getConceitosPorTurmaEDisciplina)
+router.get("/turmadisciplina", conceitoController.getConceitosPorTurmaEDisciplina)
 
-router.put("/unidade",  conceitoController.updateNotaUnidade);
+router.put("/unidade", conceitoController.updateNotaUnidade);
 
-router.put("/anual",   conceitoController.updateAnual);
+router.put("/anual", conceitoController.updateAnual);
 
 module.exports = router;
