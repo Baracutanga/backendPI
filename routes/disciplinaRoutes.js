3f09a4ed9c23f89f73872d6f93bf450761a15f64
Roger Cauã <132178311+rogercaua@users.noreply.github.com>
2024-10-10 08:17:01 -0300

    Update disciplinaRoutes.js

--- routes/disciplinaRoutes.js
+++ routes/disciplinaRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const disciplinaController = require("../controllers/disciplinaController");
 const autenticaMiddlewareCoordenador = require("../middleware/autenticaMiddlewareCoordenador")
 
-router.post("/create", autenticaMiddlewareCoordenador, disciplinaController.createDisciplina);
+router.post("/create", disciplinaController.createDisciplina);
 
-router.get("/", autenticaMiddlewareCoordenador,  disciplinaController.getAllDisciplinas);
+router.get("/", disciplinaController.getAllDisciplinas);
 
-router.delete("/delete", autenticaMiddlewareCoordenador, disciplinaController.deleteDisciplina);
+router.delete("/delete", disciplinaController.deleteDisciplina);
 
-router.delete("/delete/all", autenticaMiddlewareCoordenador, disciplinaController.deleteAllDisciplinas);
+router.delete("/delete/all", disciplinaController.deleteAllDisciplinas);
 
 module.exports = router;
