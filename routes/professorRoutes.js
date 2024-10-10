dc2e8fa62ff420454d62b8b43016334a1dabc510
Roger Cauã <132178311+rogercaua@users.noreply.github.com>
2024-10-10 08:17:37 -0300

    Update professorRoutes.js

--- routes/professorRoutes.js
+++ routes/professorRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const professorController = require("../controllers/professorController");
 const autenticaMiddlewareCoordenador = require("../middleware/autenticaMiddlewareCoordenador")
 
-router.get("/", autenticaMiddlewareCoordenador, professorController.getAllProfessores);
+router.get("/", professorController.getAllProfessores);
 
-router.post("/create", autenticaMiddlewareCoordenador, professorController.createProfessor);
+router.post("/create", professorController.createProfessor);
 
-router.delete("/delete", autenticaMiddlewareCoordenador, professorController.deleteProfessor);
+router.delete("/delete", professorController.deleteProfessor);
 
-router.put("/update", autenticaMiddlewareCoordenador, professorController.updateProfessor);
+router.put("/update", professorController.updateProfessor);
 
 module.exports = router;
