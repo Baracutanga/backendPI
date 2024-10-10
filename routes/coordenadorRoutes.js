f17596296ae100f139506067961a82cc23a224d2
Roger Cauã <132178311+rogercaua@users.noreply.github.com>
2024-10-10 08:16:30 -0300

    Update coordenadorRoutes.js

--- routes/coordenadorRoutes.js
+++ routes/coordenadorRoutes.js
@@ -4,11 +4,11 @@ const coordenadorController = require("../controllers/coordenadorController");
 const autenticaMiddlewareCoordenador = require("../middleware/autenticaMiddlewareCoordenador")
 
 // Rota para criar um coordenador
-router.post("/create", autenticaMiddlewareCoordenador, coordenadorController.createCoordenador);
+router.post("/create", coordenadorController.createCoordenador);
  
 // Rota para deletar um coordenador
-router.delete("/delete", autenticaMiddlewareCoordenador, coordenadorController.deleteCoordenador);
+router.delete("/delete", coordenadorController.deleteCoordenador);
 
-router.put("/update", autenticaMiddlewareCoordenador, coordenadorController.updateCoordenador);
+router.put("/update", coordenadorController.updateCoordenador);
 
 module.exports = router;
