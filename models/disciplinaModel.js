38096fc5b2f14c23cfed6b782939050b81aa1cfe
Roger Cauã <132178311+rogercaua@users.noreply.github.com>
2024-11-27 20:52:48 -0300

    Update disciplinaModel.js

--- models/disciplinaModel.js
+++ models/disciplinaModel.js
@@ -4,9 +4,7 @@ const disciplinaSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   professor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   turmas: [{ type: mongoose.Schema.Types.ObjectId, ref: "Turma" }] // 
-}, {
-  timestamps: true
-});
+}, { timestamps: true }, { versionKey: false });
 
 // Exportando o modelo de disciplina
 module.exports = mongoose.model("Disciplina", disciplinaSchema);
