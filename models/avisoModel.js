5887c00f30ab17d1d01a88f70134f4c56377711f
AntonioMontenegro-Glich <antoniolemos2004@gmail.com>
2024-12-01 17:11:32 -0300

    Leve alteração avisoModel

--- models/avisoModel.js
+++ models/avisoModel.js
@@ -4,7 +4,7 @@ const avisoSchema = new mongoose.Schema({
   nome: { type: String, required: true },  
   descricao: { type: String, required: true }, 
   turma: { type: [mongoose.Schema.Types.ObjectId], ref: "Turma", required: true },    
-  disciplina: { type: mongoose.Schema.Types.ObjectId, ref: "Disciplina", required: true },
+  disciplina: { type: mongoose.Schema.Types.ObjectId, ref: "Disciplina", required: false },
   autor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true } 
 }, { timestamps: true }, { versionKey: false });
 
