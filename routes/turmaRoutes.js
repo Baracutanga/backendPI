965978fe5a9bf83094010ec3a1ddc795125db31c
Roger Cauã <132178311+rogercaua@users.noreply.github.com>
2024-12-05 02:42:22 -0300

    Update turmaRoutes.js

--- routes/turmaRoutes.js
+++ routes/turmaRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const turmaController = require('../controllers/turmaController');
-const autenticaMiddlewareCoordenador = require("../middleware/autenticaMiddlewareCoordenador")
+const autenticaMiddlewareCoordenador = require("../middleware/autenticaMiddlewareCoordenador");
+const autenticaMiddlewareAluno = require("../middleware/autenticaMiddlewareAluno");
 
 router.post('/create', autenticaMiddlewareCoordenador, turmaController.createTurma);
 
 router.get('/', autenticaMiddlewareCoordenador, turmaController.getAllTurmas);
+
+router.get('/horario', autenticaMiddlewareAluno, turmaController.getHorarioAula);
+
+router.put('/horario', autenticaMiddlewareAluno, turmaController.updateHorarioAula);
 
 router.delete('/delete/:id?', autenticaMiddlewareCoordenador, turmaController.deleteTurma);
 
